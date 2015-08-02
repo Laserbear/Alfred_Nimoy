40963eb32d683c1b18218e8d1cf0e942f67bf012
Christian Ng <christianr.ng@gmail.com>
2015-08-02 02:42:55 -0700

    Create mongodb.py

---
+++ mongodb.py
@@ -0,0 +1,13 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+
+db = conn.['Nimoy'] #http://altons.github.io/python/2013/01/21/gentle-introduction-to-mongodb-using-pymongo/
+brain = db.brain
+
+def learn(trigger, response): #find way to add association if trigger isnt found
+  data = {}
+  data['trigger'] = trigger
+  data['response'] = response #find way to add new responses
+  brain.insert(data )
