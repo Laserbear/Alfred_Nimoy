312da2ebc40ed951cf16aac7cb808aa5c0c91b99
Christian Ng <christianr.ng@gmail.com>
2015-08-02 01:44:51 -0700

    Set up connection

---
+++ Nimoy.py
@@ -0,0 +1,35 @@
+import socket
+
+#connection details
+bot_owner = "Laserbear"
+nick = "Alfred_Nimoy"
+chan = "#Nimoy"
+sock = socket.socket()
+sock.connect(("irc.foonetic.net",6667))
+sock.send("USER " + nick + " 0 * :" + bot_owner + "\r\n")
+sock.send("NICK " + nick + "\r\n")
+
+def databasecheck(data): #checks whether data contains trigger
+  pass
+
+def trigger_response(trigger): #selects random response corresponding to trigger
+  pass
+  
+def teachable(data): #determines whether a trigger response can be learned
+  pass
+  
+replied = 0
+while 1:
+   replied = 0
+   data = sock.recv(1024)
+   print data
+   if data[0:4] == "PING":
+      sock.send(data.replace("PING", "PONG"))
+   if data[0]!=':':
+      continue
+   if data.split(" ")[1] == "001":
+      sock.send("MODE " + nick + " +B\r\n")
+      sock.send("JOIN " + chan + "\r\n")
+  if databasecheck(data)[0] == 1 and not teachable(data):
+    sock.send("PRIVMSG " + chan + " :databasecheck(data)[1]\r\n")
+    
